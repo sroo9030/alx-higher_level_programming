40e5adc7c16a36faa73110bdcc2c3f2eae17abd7
esra <eisra3000ese@gmail.com>
2024-03-28 13:13:33 -0700

    1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- a script that creates a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
