9befffb59b6b8baa564096c10ade5947bb8529d3
esra <eisra3000ese@gmail.com>
2024-07-30 12:01:49 -0700

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,15 +2,9 @@
 
 const process = require('process');
 const args = process.argv;
-let count = 0;
 
-args.forEach((val, index) => {
-  count++;
-  if (count === 3) {
-    console.log(`${val}`);
-  }
-});
-
-if (count !== 3) {
+if (args[2]) {
+  console.log(args[2]);
+} else {
   console.log('No argument');
 }
