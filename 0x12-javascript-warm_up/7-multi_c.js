8bbd68d27de49384ec0bff32d1af2515e68f813e
lenovo <eisra3000ese@gmail.com>
2024-07-31 00:40:34 +0300

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ const args = process.argv;
 
 const num = Math.floor(parseFloat(args[2]));
 
-for (let i = 0; i < num; i++){
- console.log('C is fun');
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
 }
