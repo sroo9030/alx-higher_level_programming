54bdf887c7ff7f28245db04159326b026bfa9c3c
lenovo <eisra3000ese@gmail.com>
2024-07-31 01:05:21 +0300

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ const num = Math.floor(parseFloat(args[2]));
 
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
-    console.log('x'.repeat(num));
+    console.log('X'.repeat(num));
   }
 } else {
   console.log('Missing size');
