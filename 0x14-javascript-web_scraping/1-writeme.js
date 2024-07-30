a266c45e8d479d42d5951fa41174b11a8bf19f24
lenovo <eisra3000ese@gmail.com>
2024-07-31 01:51:02 +0300

    1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+if (filePath && stringToWrite) {
+  fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+} else {
+  console.log('Missing file path or string to write');
+}
